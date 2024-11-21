a0909594c8fd13ac087b866cdaafcbd68e147734
jadexaa <10987469@uvu.edu>
2024-11-20 22:36:21 -0700

    Add files via upload

---
+++ data/customers.js
@@ -0,0 +1,18 @@
+export const customers = [
+    {
+        "name": "Autumn L - ★★★★★",
+        "review": "Wonderful experience, traded my car in with no hassle or pressure",
+    },
+    {
+        "name": "Abby L - ★★★★★",
+        "review": "I had Richard as my salesman, he was very transparent and knowledgable about the cars he recommended",
+    },
+    {
+        "name": "Ally L - ★★★★★",
+        "review": "Had my oil changed, it was done in under an hour. A quick in a out at a good rate",
+    },
+    {
+        "name": "Brenda X - ★★★★★",
+        "review": "Had problems with my car, brought it in and it was fixed",
+    },
+]
